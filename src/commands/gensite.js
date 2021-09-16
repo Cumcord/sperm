c03ea7419e31b2eb30932dc56d97ae171bf70dc9
Alyxia Sother <lexisoth2005@gmail.com>
2021-09-16 20:57:32 +0200

    Raw, unstyled procreation.

---
+++ src/commands/gensite.js
@@ -0,0 +1,28 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+async function gensite(args) {
+  console.log('Building site...');
+
+  await fs.access(args.manifest).catch(() => {
+    throw new Error(`${args.manifest} does not exist`);
+  });
+
+  let manifestJson;
+  try {
+    manifestJson = JSON.parse(await fs.readFile(args.manifest, 'utf8'));
+  } catch {
+    throw new Error(`${args.manifest} is not valid json`);
+  }
+
+  let html =
+    '<html><head><title>[TITLE]</title></head><body><h1>[TITLE]</h1><h2>[DESC]</h2></body></html>';
+  html = html.replace(/\[TITLE\]/g, manifestJson.name);
+  html = html.replace(/\[DESC\]/g, manifestJson.description);
+
+  await fs.writeFile(path.join(args.outdir, 'index.html'), html);
+
+  console.log(`Wrote static site to ${path.join(args.outdir, '/')}.`);
+}
+
+module.exports = gensite;
--- src/index.js
+++ src/index.js
@@ -1,115 +1,137 @@
 #!/usr/bin/env node
 
-const yargs = require("yargs");
-const inquirer = require("inquirer");
+const yargs = require('yargs');
+const inquirer = require('inquirer');
 
 // require commands
-const init = require("./commands/init");
-const build = require("./commands/build");
-const dev = require("./commands/dev");
+const init = require('./commands/init');
+const build = require('./commands/build');
+const dev = require('./commands/dev');
+const gensite = require('./commands/gensite');
 
 const argumentParser = yargs
-  .scriptName("sperm")
-  .usage("$0 <cmd> [args]")
+  .scriptName('sperm')
+  .usage('$0 <cmd> [args]')
   .help()
-  .alias("h", "help")
+  .alias('h', 'help')
   .strict()
   .demand(1);
 
 // init command
 argumentParser.command(
-  "init [outfile]",
-  "Creates a Cumcord manifest file at [outfile]",
+  'init [outfile]',
+  'Creates a Cumcord manifest file at [outfile]',
 
   (yargs) => {
     // plugin name option
-    yargs.positional("outfile", {
-      type: "string",
-      default: "cumcord_manifest.json",
+    yargs.positional('outfile', {
+      type: 'string',
+      default: 'cumcord_manifest.json',
     });
 
-    yargs.option("name", {
-      alias: "n",
-      type: "string",
+    yargs.option('name', {
+      alias: 'n',
+      type: 'string',
       default: undefined,
-      describe: "the name of the plugin",
+      describe: 'the name of the plugin',
     });
 
     // plugin description option
-    yargs.option("description", {
-      alias: "d",
-      type: "string",
+    yargs.option('description', {
+      alias: 'd',
+      type: 'string',
       default: undefined,
-      describe: "the description of the plugin",
+      describe: 'the description of the plugin',
     });
 
     // author option
-    yargs.option("a", {
-      alias: "author",
-      type: "string",
+    yargs.option('a', {
+      alias: 'author',
+      type: 'string',
       default: undefined,
-      describe: "the author of the plugin",
+      describe: 'the author of the plugin',
     });
 
     // main file option
-    yargs.option("f", {
-      alias: "file",
-      type: "string",
+    yargs.option('f', {
+      alias: 'file',
+      type: 'string',
       default: undefined,
-      describe: "the main file of the plugin",
+      describe: 'the main file of the plugin',
     });
 
     // license option
-    yargs.option("l", {
-      alias: "license",
-      type: "string",
+    yargs.option('l', {
+      alias: 'license',
+      type: 'string',
       default: undefined,
-      describe: "the license of the plugin",
+      describe: 'the license of the plugin',
     });
   },
   (args) => {
     return init(args);
-  }
+  },
 );
 
 // build command
 argumentParser.command(
-  "build [manifest]",
-  "Builds a Cumcord plugin from a [manifest] file",
+  'build [manifest]',
+  'Builds a Cumcord plugin from a [manifest] file',
   (yargs) => {
-    yargs.positional("manifest", {
-      type: "string",
-      default: "cumcord_manifest.json",
-      describe: "a cumcord manifest file",
+    yargs.positional('manifest', {
+      type: 'string',
+      default: 'cumcord_manifest.json',
+      describe: 'a cumcord manifest file',
     });
 
     // outfile option
-    yargs.option("o", {
-      alias: "outdir",
-      type: "string",
-      default: "dist",
+    yargs.option('o', {
+      alias: 'outdir',
+      type: 'string',
+      default: 'dist',
     });
   },
-  build
+  build,
 );
 
+// dev command
 argumentParser.command(
-  "dev [manifest]",
-  "Hot-rebuilds your plugin and sends it to the client",
+  'dev [manifest]',
+  'Hot-rebuilds your plugin and sends it to the client',
   (yargs) => {
-    yargs.positional("manifest", {
-      type: "string",
-      default: "cumcord_manifest.json",
-      describe: "a cumcord manifest file",
+    yargs.positional('manifest', {
+      type: 'string',
+      default: 'cumcord_manifest.json',
+      describe: 'a cumcord manifest file',
     });
-    yargs.option("p", {
-      alias: "port",
-      type: "number",
+    yargs.option('p', {
+      alias: 'port',
+      type: 'number',
       default: 6463,
-      describe: "the port to connect to"
+      describe: 'the port to connect to',
     });
   },
-  dev
+  dev,
+);
+
+// sitegen
+argumentParser.command(
+  'sitegen [manifest]',
+  'Builds a static site for your plugin',
+  (yargs) => {
+    yargs.positional('manifest', {
+      type: 'string',
+      default: 'cumcord_manifest.json',
+      describe: 'a cumcord manifest file',
+    });
+
+    yargs.option('o', {
+      alias: 'outdir',
+      type: 'string',
+      default: 'dist',
+    });
+  },
+  gensite,
 );
 
 argumentParser.argv;
